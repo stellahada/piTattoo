19b27dfe30699fbfba091f1702b835331e766257
stellahada <stellacavalcante2004@gmail.com>
2024-11-11 11:25:43 -0300

    conexao com o backEnd

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,38 +1,49 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; // Importe o módulo HttpClientModule
 import { API_URL } from '../../app/api/constants'
 
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  standalone: true,
+  imports: [ FormsModule, HttpClientModule],
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent {
   cpf: string = '';
   senha: string = '';
 
   constructor(private http: HttpClient, private router: Router) {}
 
-  async logar() {
+  logar() {
     const body = {
       cpf: this.cpf,
       senha: this.senha
     };
-
-    try {
-      const resp: any = await this.http.post('http://localhost:3010/login', body).toPromise();
-      if (resp && resp.token) {
-        localStorage.setItem('TOKEN', resp.token);
-        this.router.navigate(['/home']);
-      } else {
-        console.error("A resposta não possui dados.", resp);
-        alert("Erro ao fazer login, por favor tente novamente.");
+  
+    console.log("Enviando dados:", body); // Verifique os dados que estão sendo enviados
+  
+    this.http.post('http://localhost:8080/login/l', body, {
+      headers: { 'Content-Type': 'application/json' }
+    }).subscribe({
+      next: (resp: any) => {
+        console.log("Resposta recebida:", resp);
+        if (resp) {
+          this.router.navigate(['/about']);
+        } else {
+          alert("Erro ao fazer login, por favor tente novamente.");
+        }
+      },
+      error: (err: any) => {
+        console.error("Erro na requisição:", err);
+        alert(err.error?.erro || "Erro ao fazer login, por favor tente novamente.");
       }
-    } catch (err: any) {
-      console.error("Erro na requisição:", err);
-      alert(err.error?.erro || "Erro ao fazer login, por favor tente novamente.");
-    }
+    });
   }
+  
 }
